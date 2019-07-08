11c785c9b7236aedaa3c64e8777ecc92772ce9d1
Jens Christian Jensen <jensecj@gmail.com>
2019-07-08 15:11:33 +0200

    added skeleton

---
+++ straight-ui.el
@@ -0,0 +1,21 @@
+;;; straight-ui.el --- Some description. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Jens Christian Jensen
+
+;; Author: Jens Christian Jensen <jensecj@gmail.com>
+;; URL:
+;; Keywords:
+;; Package-Requires ((emacs "27.0.50"))
+;; Package-Version: 20190708
+;; Version: 0.1.0
+
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+
+
+(provide 'straight-ui)
+;;; straight-ui.el ends here
